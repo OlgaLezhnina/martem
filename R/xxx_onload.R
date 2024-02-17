d924133491db4f82c51a02ddeb61007bacf208de
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-02-17 12:56:13 +0100

    delete onload

--- R/xxx_onload.R
+++
@@ -1,8 +0,0 @@
-#.onLoad <- function(libname, pkgname) {
-#  the$templ_info <-
-#    jsonlite::fromJSON(system.file("extdata", "measurement_orkg.json", package = "martem"))
-#  the$templ_class <-
-#    set_class(readLines(
-#      system.file("extdata", "measurement_orkg.json", package = "martem")
-#    ))
-#}
