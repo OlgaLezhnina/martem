59016be4f0839b6bb1ab26a837c11778f95994e0
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-02-17 13:29:31 +0100

    replace extractor function to load_refernce_classes file

--- R/load_reference_classes.R
+++ R/load_reference_classes.R
@@ -1,6 +1,71 @@
 loaded_templates <- new.env()
 
-#' Set a reference class(es) for a template and its components
+#' Extractor function for the ORKG templates
+#'
+#' @param template_id
+#'
+#' @return
+#'
+#' @examples
+#'
+extractor_orkg <- function(template_id) {
+  extract_all <- list()
+  extractor_function <- function(template_id) {
+    info <-
+      request_orkg(paste("/api/templates/", template_id, sep = ""))
+    template_name <- format_string(info$label)
+    template_class <- info$target_class
+    templ_df <- data.frame(template_name, template_class)
+    i <- 0
+    all_comps <- data.frame(
+      predicate_id = character(),
+      predicate_label = character(),
+      value_class_id = character(),
+      nested_template = logical(),
+      stringsAsFactors = FALSE
+    )
+    for (component in info$properties) {
+      predicate_id <- component$path$id
+      predicate_label <- component$path$label
+      if (is.null(component$class$id)) {
+        value_class_id <- component$datatype$id
+        nested_template <- FALSE
+      } else {
+        value_class_id <- component$class$id
+        info_n <- request_orkg(paste(
+          "/api/templates/?target_class=",
+          value_class_id,
+          sep = ""
+        ))
+        if (!length(info_n$content)) {
+          nested_template <- FALSE
+        } else {
+          nested_template <- TRUE
+          nested_id <- info_n$content[[1]]$id
+          nested_name <- info_n$content[[1]]$label
+          if (!nested_name %in% names(extract_all)) {
+            extractor_function(nested_id)
+          }
+        }
+      }
+      comp_list <- list(predicate_id,
+                        predicate_label,
+                        value_class_id,
+                        nested_template)
+      i <- i + 1
+      all_comps[i, ] <- comp_list
+      all_comps <- all_comps[order(all_comps$predicate_id), ]
+    }
+    extracted <-
+      list(templ_df, all_comps)
+    extract_all[[template_name]] <<- list(extracted)
+    return(extract_all)
+  }
+  extractor_function(template_id)
+  return(extract_all)
+}
+
+#' Load reference class(es) for a template and its components
 #'
 #' @param template_id
 #'
