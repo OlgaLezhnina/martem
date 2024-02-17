11e55943b317b5892fe9139e284f64bb5f05b4b5
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-02-17 21:25:16 +0100

    add unit tests

--- tests/testthat/test-load_reference_classes.R
+++ tests/testthat/test-load_reference_classes.R
@@ -15,3 +15,27 @@ test_that("extractor_orkg works", {
   "")
   expect_equal(string_representation, expected)
 })
+
+test_that("load_reference_classes works", {
+  result_load = load_reference_classes("R937648")
+  string_load = capture.output(print(result_load))
+  expected_load = c(
+    "$measurement_scale",
+    "Generator for class \"measurement_scale_orkg\":",
+    "",
+    "Class fields:",
+    "                                                                  ",
+    "Name:           label  template_name template_class     components",
+    "Class:      character      character      character           list",
+    "",
+    "Class Methods: ",
+    "     \"initialize\", \"field\", \"trace\", \"getRefClass\", \"initFields\", \"copy\", ",
+    "     \"callSuper\", \".objectPackage\", \"export\", \"untrace\", \"getClass\", \"show\", ",
+    "     \"usingMethods\", \".objectParent\", \"import\"",
+    "",
+    "Reference Superclasses: ",
+    "     \"envRefClass\"",
+    "",
+    "")
+  expect_equal(string_load, expected_load)
+})
