c86d5daa1d43507fc5e2eef61c2b0635d7c21249
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-02-18 17:38:34 +0100

    unit test

--- tests/testthat/test-helpers.R
+++ tests/testthat/test-helpers.R
@@ -1,3 +1,8 @@
 test_that("format_string works", {
   expect_equal(format_string("a-b c"), "a_b_c")
 })
+
+test_that("show_fields works", {
+  tp <- load_reference_classes("R937648")
+expect_equal(show_fields(tp$measurement_scale), "label")
+})
